bfd48933f1cf9cddd1aaa0b24c930fe78c8cfd0e
hmaghzaz <h.maghzaz@gmail.com>
2020-03-29 14:23:09 +0100

    initial

---
+++ assets/scripts/app.js
@@ -0,0 +1,9 @@
+const defaultResult = 0;
+let currentResult = defaultResult;
+
+currentResult += 10;
+
+let calucationDescription = `(${defaultResult} + 10)`;
+let errorMessage = "An error " + "is occurred!";
+
+outputResult(currentResult, calucationDescription);
