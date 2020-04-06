dd6d25e1cbee847c3b8f16505fcf704b288388d8
hmaghzaz <h.maghzaz@gmail.com>
2020-04-06 15:48:42 +0100

    L38 refactoring implementation of add function and add new function to get user number input

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,10 +1,17 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
+//function convert to int the input user
+function getUserNumberInput() {
+  return parseInt(userInput.value);
+}
+
 //Funciton addition
 function add() {
-  currentResult = currentResult + parseInt(userInput.value);
-  outputResult(currentResult, "");
+  const entredNumber = getUserNumberInput();
+  const calcDescription = `${currentResult} + ${entredNumber}`;
+  currentResult = currentResult + entredNumber;
+  outputResult(currentResult, calcDescription);
 }
 
 //listening to event add buton
