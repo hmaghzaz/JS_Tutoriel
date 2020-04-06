44a86f66b5d8635b15c1427aa1fadc6c30a317f1
hmaghzaz <h.maghzaz@gmail.com>
2020-04-06 16:44:41 +0100

    L39 refactoring add function and creating new function - * / and there event listener and create a new function to write the operation called createAndWriteOutput

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -6,13 +6,43 @@ function getUserNumberInput() {
   return parseInt(userInput.value);
 }
 
+//Function to write to calculation operation
+function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
+  const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
+  outputResult(currentResult, calcDescription);
+}
+
 //Funciton addition
 function add() {
   const entredNumber = getUserNumberInput();
-  const calcDescription = `${currentResult} + ${entredNumber}`;
+  const intialResutl = currentResult;
   currentResult = currentResult + entredNumber;
-  outputResult(currentResult, calcDescription);
+  createAndWriteOutput("+", intialResutl, entredNumber);
+}
+
+function subtract() {
+  const entredNumber = getUserNumberInput();
+  const intialResutl = currentResult;
+  currentResult = currentResult - entredNumber;
+  createAndWriteOutput("-", intialResutl, entredNumber);
+}
+
+function multiply() {
+  const entredNumber = getUserNumberInput();
+  const intialResutl = currentResult;
+  currentResult = currentResult * entredNumber;
+  createAndWriteOutput("*", intialResutl, entredNumber);
+}
+
+function devide() {
+  const entredNumber = getUserNumberInput();
+  const intialResutl = currentResult;
+  currentResult = currentResult / entredNumber;
+  createAndWriteOutput("/", intialResutl, entredNumber);
 }
 
 //listening to event add buton
 addBtn.addEventListener("click", add);
+subtractBtn.addEventListener("click", subtract);
+multiplyBtn.addEventListener("click", multiply);
+divideBtn.addEventListener("click", devide);
